98ec275094d8ef20756ba31eace8163c46d4ba69
Pepe <pgarciag93@gmail.com>
2023-04-28 10:09:17 +0200

    Sidecar init

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Sidecar\OgImage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('sidecar', function () {
+  return OgImage::execute([
+    'text' => 'PHP to JS and Back Again!'
+]);
 });
